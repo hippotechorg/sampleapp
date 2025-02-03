4f8cb6a2092d322c86eed0ce368037be036e306f
Jouni Lehto <lehto.jouni@gmail.com>
2025-02-03 14:56:08 +0200

    run full BD scan

--- src/main/java/com/example/main/SampleMain.java
+++ src/main/java/com/example/main/SampleMain.java
@@ -3,7 +3,6 @@ package com.example.main;
 public class SampleMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Testing JAVA_OPTS");
 		System.out.println("Testi: " + System.getProperty("testi2"));
 
